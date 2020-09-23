3392ed76b5194545e246c6661a02ef71a22fc666
Pham Quynh <thuyquynh.toki@gmail.com>
2020-09-22 21:46:07 -0500

    Start new project

---
+++ question-api/src/main/java/com/studybuddy/main/QuestionAPIMain.java
@@ -0,0 +1,11 @@
+package com.studybuddy.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionAPIMain {
+    public static void main(String[] args) {
+        SpringApplication.run(QuestionAPIMain.class, args);
+    }
+}
